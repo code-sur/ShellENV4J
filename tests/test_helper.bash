2d9d6ab0eb4cf1ac20e702220a6eed1ef3539a5a
Code Raguet <ignacio.code@gmail.com>
2016-10-01 20:53:30 -0300

    Add funtion "assert_output"

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -1,9 +1,23 @@
 #!/usr/bin/env bats
 
+UNDERLINE='\e[4m'
+NO_UNDERLINE='\e[24m'
+
 
 assert_success() {
   if [ "$status" -ne 0 ]; then
     echo "command failed with exit status $status"
+    return 1
+  fi
+}
+
+
+assert_output() {
+  expected=$1
+  actual=$output
+  if [ "$actual" != "$expected" ]; then
+    echo -e "${UNDERLINE}expected:${NO_UNDERLINE} $expected"
+    echo -e "${UNDERLINE}actual:${NO_UNDERLINE} $actual"
     return 1
   fi
 }
