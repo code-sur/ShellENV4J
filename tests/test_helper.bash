fa49ea7d3841911ab20163b80b6247d5c169423e
Code Raguet <ignacio.code@gmail.com>
2016-10-01 20:56:55 -0300

    Underline fail message of assert_success

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -6,7 +6,7 @@ NO_UNDERLINE='\e[24m'
 
 assert_success() {
   if [ "$status" -ne 0 ]; then
-    echo "command failed with exit status $status"
+    echo -e "${UNDERLINE}command failed with exit status $status"
     return 1
   fi
 }
